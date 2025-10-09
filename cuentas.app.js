9f37e1c8837cac2579a4d5a4aef0bb485867ee22
DARZP <diego.arias123@gmail.com>
2025-10-09 12:34:46 -0600

    Update cuentas.app.js

--- cuentas.app.js
+++ cuentas.app.js
@@ -1,15 +1,4 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA4zRiQnr2PiG1zQc_k-Of9CmGQQSkVQ84",
-    authDomain: "finztone-app.firebaseapp.com",
-    projectId: "finztone-app",
-    storageBucket: "finztone-app.appspot.com",
-    messagingSenderId: "95145879307",
-    appId: "1:95145879307:web:e10017a75edf32f1fde40e",
-    measurementId: "G-T8KMJXNSTP"
-};
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+import { auth, db } from './firebase-init.js';
 
 const addAccountForm = document.getElementById('add-account-form');
 const accountsListContainer = document.getElementById('accounts-list');
