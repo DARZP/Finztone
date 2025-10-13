d2579e497f89da72ea1fe4e53825e7b978ebf3f1
DARZP <diego.arias123@gmail.com>
2025-10-13 15:52:16 -0600

    Update reportes.app.js

--- reportes.app.js
+++ reportes.app.js
@@ -157,28 +157,3 @@ generateBtn.addEventListener('click', async () => {
     }
 });
 
-function exportToCSV(data) {
-    const headers = Object.keys(data[0]);
-    const csvRows = [
-        headers.join(','),
-        ...data.map(row => 
-            headers.map(fieldName => 
-                JSON.stringify(row[fieldName], (key, value) => value === null ? '' : value)
-            ).join(',')
-        )
-    ];
-
-    const csvString = csvRows.join('\n');
-    const blob = new Blob(['\uFEFF' + csvString], { type: 'text/csv;charset=utf-8;' });
-
-    const link = document.createElement('a');
-    if (link.download !== undefined) {
-        const url = URL.createObjectURL(blob);
-        link.setAttribute('href', url);
-        link.setAttribute('download', `Reporte-Finztone-${new Date().toISOString().split('T')[0]}.csv`);
-        link.style.visibility = 'hidden';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
-}
