909bc978fb6a32ead0a1a7269d5cf5243c512b2b
DARZP <diego.arias123@gmail.com>
2026-04-08 00:25:29 -0600

    Update empleado_gastos.app.js

--- empleado_gastos.app.js
+++ empleado_gastos.app.js
@@ -384,6 +384,7 @@ function mostrarGastos(gastos) {
 function poblarFiltrosYCategorias() {
     monthFilter.innerHTML = '<option value="todos">Todos los meses</option>';
     let fecha = new Date();
+    const currentMonthValue = `${fecha.getFullYear()}-${String(fecha.getMonth() + 1).padStart(2, '0')}`;
     for (let i = 0; i < 12; i++) {
         const value = `${fecha.getFullYear()}-${String(fecha.getMonth() + 1).padStart(2, '0')}`;
         const text = fecha.toLocaleString('es-ES', { month: 'long', year: 'numeric' });
